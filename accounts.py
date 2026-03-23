9308877890c7be312bd61019e60c88289766b1fb
lajan <aiswarialajan124@gmail.com>
2026-03-23 15:07:28 +0000

    Weekly Task 03 - completed accounts.py

--- accounts.py
+++ accounts.py
@@ -1,7 +1,7 @@
 # Asks to enter 10 digit number
 account_number = input("Enter 10 digit account number: ")
 
-# The last four digits of number inputtes is shown, the other numbers are hidden with X
+# The last four digits of number inputted is shown, the other numbers are hidden with X
 if len(account_number) <= 4:
     masked = account_number
 else:
